a6619736b6f7a925581fdbdef6059d2ed0f4c082
lmf2003 <92583927+lmf2003@users.noreply.github.com>
2021-10-25 13:01:02 +0800

    Add files via upload

---
+++ 第一题.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int a,n;
+scanf("%d",&a);
+for(n=1;a>10;n++){
+	a/=10;
+}
+	printf("ÊÇ%dÎ»Êý",n);
+	for() 
+   
+}
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	for(a=1;a<5;a++){
+		for(b=1;b<5;b++){
+			for(c=1;c<5;c++){
+if(a!=b){
+ if(b!=c){
+  if(c!=a){
+	 printf("%d%d%d ",a,b,c);
+	        }
+          }
+       	}
+			}
+		}
+	}
+}
+
