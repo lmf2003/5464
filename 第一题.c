4efcce872e5d386bb19379d295a1060cfa0b5b49
lmf2003 <92583927+lmf2003@users.noreply.github.com>
2021-10-27 20:38:35 +0800

    Update 第一题.c

--- 第一题.c
+++ 第一题.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
+int a,b,c,n;
 	for(a=1;a<5;a++){
 		for(b=1;b<5;b++){
 			for(c=1;c<5;c++){
-if(a!=b){
- if(b!=c){
-  if(c!=a){
-	 printf("%d%d%d ",a,b,c);
-	        }
-          }
-       	}
-			}
+				if(a!=b&&a!=c&&b!=c){
+					n++;
+				printf("%d%d%d ",a,b,c);
+			 }
+		   }
 		}
 	}
-}
-
+	printf("能组成%d个",n);
+ } 
+ 
