954d8110c0757fa359b16eb1c3776654b84507a8
lmf2003 <92583927+lmf2003@users.noreply.github.com>
2021-10-27 19:14:26 +0800

    Update 第一题.c

--- 第一题.c
+++ 第一题.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
 int main()
 {
-	int a,n;
-scanf("%d",&a);
-for(n=1;a>10;n++){
-	a/=10;
-}
-	printf("ÊÇ%dÎ»Êý",n);
-	for() 
-   
-}
-
-#include<stdio.h>
-int main()
-{
 	int a,b,c;
 	for(a=1;a<5;a++){
 		for(b=1;b<5;b++){
